02654aa382f3b5ba6d3e9dca804183a1fbf62d03
Andrei <abartasevich@vtb.ru>
2020-12-06 12:18:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 50;
+        //кол-во литров заправленного топлива
+        int b = 43;
+        //цена за  литр топлива
+        int c = a*b;
+        //стоимость заправки
+
+        System.out.println(c);
+    }
+}
